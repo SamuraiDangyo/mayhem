f587848d60b9e945d676defffb47576ca7695df9
Toni Helminen <kalleankka1@gmail.com>
2020-12-08 21:12:13 +0200

    Add files via upload

--- lib/nnue.cpp
+++ lib/nnue.cpp
@@ -32,6 +32,126 @@ extern "C" {
 #include "nnue.hpp"
 #undef DLL_EXPORT
 
+// misc.cpp start
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include <inttypes.h>
+
+#ifdef _WIN32
+#  include <windows.h>
+#else
+#  include <sys/mman.h>
+#endif
+
+#if defined (__GNUC__)
+#   define INLINE  __inline __attribute__((always_inline))
+#elif defined (_WIN32)
+#   define INLINE  __forceinline
+#else
+#   define INLINE  __inline
+#endif
+
+#ifdef _WIN32
+
+typedef HANDLE FD;
+#define FD_ERR INVALID_HANDLE_VALUE
+typedef HANDLE map_t;
+
+#else /* Unix */
+
+typedef int FD;
+#define FD_ERR -1
+typedef size_t map_t;
+
+#endif
+
+FD open_file(const char *name);
+void close_file(FD fd);
+size_t file_size(FD fd);
+const void *map_file(FD fd, map_t *map);
+void unmap_file(const void *data, map_t map);
+
+#define clamp(a, b, c) ((a) < (b) ? (b) : (a) > (c) ? (c) : (a))
+
+FD open_file(const char *name)
+{
+#ifndef _WIN32
+  return open(name, O_RDONLY);
+#else
+  return CreateFile(name, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING,
+      FILE_FLAG_RANDOM_ACCESS, NULL);
+#endif
+}
+
+void close_file(FD fd)
+{
+#ifndef _WIN32
+  close(fd);
+#else
+  CloseHandle(fd);
+#endif
+}
+
+size_t file_size(FD fd)
+{
+#ifndef _WIN32
+  struct stat statbuf;
+  fstat(fd, &statbuf);
+  return statbuf.st_size;
+#else
+  DWORD sizeLow, sizeHigh;
+  sizeLow = GetFileSize(fd, &sizeHigh);
+  return ((uint64_t)sizeHigh << 32) | sizeLow;
+#endif
+}
+
+const void *map_file(FD fd, map_t *map)
+{
+#ifndef _WIN32
+
+  *map = file_size(fd);
+  void *data = mmap(NULL, *map, PROT_READ, MAP_SHARED, fd, 0);
+#ifdef MADV_RANDOM
+  madvise(data, *map, MADV_RANDOM);
+#endif
+  return data == MAP_FAILED ? NULL : data;
+
+#else
+
+  DWORD sizeLow, sizeHigh;
+  sizeLow = GetFileSize(fd, &sizeHigh);
+  *map = CreateFileMapping(fd, NULL, PAGE_READONLY, sizeHigh, sizeLow, NULL);
+  if (*map == NULL)
+    return NULL;
+  return MapViewOfFile(*map, FILE_MAP_READ, 0, 0, 0);
+
+#endif
+}
+
+void unmap_file(const void *data, map_t map)
+{
+  if (!data) return;
+
+#ifndef _WIN32
+
+  munmap((void *)data, map);
+
+#else
+
+  UnmapViewOfFile(data);
+  CloseHandle(map);
+
+#endif
+}
+
+// misc.cpp end
+
 // Old gcc on Windows is unable to provide a 32-byte aligned stack.
 // We need to hack around this when using AVX2 and AVX512.
 #if     defined(__GNUC__ ) && (__GNUC__ < 9) && defined(_WIN32) \
@@ -1136,6 +1256,4 @@ DLLExport int _CDECL nnue_evaluate(int player, int* pieces, int* squares)
   pos.pieces = pieces;
   pos.squares = squares;
   return nnue_evaluate_pos(&pos);
-}
-
-}
+}}
