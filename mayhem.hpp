5286084e6f2ad727025b64ca622d31637616adde
Toni Helminen <kalleankka1@gmail.com>
2024-11-27 23:00:12 +0200

    Add files via upload

--- mayhem.hpp
+++ mayhem.hpp
@@ -462,12 +462,12 @@ inline int CtzrPop(std::uint64_t *b) {
 
 // X axle of board
 inline int MakeX(const int sq) {
-  return sq % 8;
+  return int(sq % 8);
 }
 
 // Y axle of board
 inline int MakeY(const int sq) {
-  return sq / 8;
+  return int(sq / 8);
 }
 
 // Nodes Per Second
@@ -482,12 +482,30 @@ bool IsOnBoard(const int x, const int y) {
 
 // X-coord to char
 char MakeFile2Char(const int f) {
-  return 'a' + f;
+  switch (f) {
+    case 0:  return 'a';
+    case 1:  return 'b';
+    case 2:  return 'c';
+    case 3:  return 'd';
+    case 4:  return 'e';
+    case 5:  return 'f';
+    case 6:  return 'g';
+    default: return 'h';
+  }
 }
 
 // Y-coord to char
 char MakeRank2Char(const int r) {
-  return '1' + r;
+  switch (r) {
+    case 0:  return '1';
+    case 1:  return '2';
+    case 2:  return '3';
+    case 3:  return '4';
+    case 4:  return '5';
+    case 5:  return '6';
+    case 6:  return '7';
+    default: return '8';
+  }
 }
 
 // Convert int coords to string
